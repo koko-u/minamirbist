e0328fd568d939451f546ebb596b0727aee86b36
koko_u <kozaki.tsuneaki@gmail.com>
2011-03-17 13:05:06 +0900

    コントローラのテストを多少書いた

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -4,27 +4,40 @@ require 'spec_helper'
 describe EventsController do
   before do
     @kato = Factory.create(:member, :name => 'kato')
-    @event = Factory.build(:event, :name => 'just for fun')
+    @wada = Factory.create(:member, :name => 'wada')
+    @kato_event = Factory.create(:event, :name => 'kato\'s event',
+                                 :organizer => @kato)
+    @wada_event = Factory.create(:event, :name => 'wada\'s event',
+                                 :organizer => @wada)
     login_as(@kato)
-  end
-
-  def mock_event(stubs={})
-    @mock_event ||= mock_model(Event, stubs).as_null_object
   end
 
   describe "GET index" do
     it "@events からすべての event が得られる" do
-      Event.expects(:all).returns([@event])
       get :index
-      assigns(:events).should == [@event]
+      assigns(:events).should =~ [@kato_event, @wada_event]
     end
   end
 
   describe "GET show" do
     it "@event に必要となる event 情報が含まれている" do
-      Event.expects(:find).with("37").returns(@event)
+      Event.expects(:find).with("37").returns(@kato_event)
       get :show, :id => "37"
-      assigns(:event).should == @event
+      assigns(:event).should == @kato_event
+    end
+  end
+
+  describe "GET for_organizer" do 
+    it "異なる view を呼びたいだけで動きは show と同じ" do 
+      Event.expects(:find).with("37").returns(@kato_event)
+      get :for_organizer, :id => "37"
+      assigns(:event).should == @kato_event
+    end
+
+    it "自分が主催するイベントでなければイベント一覧にリダイレクトする" do 
+      Event.expects(:find).with("42").returns(@wada_event)
+      get :for_organizer, :id => "42"
+      response.should redirect_to events_path
     end
   end
 
@@ -36,122 +49,126 @@ describe EventsController do
   end
 
   describe "GET edit" do
-    it "assigns the requested event as @event" do
-      Event.expects(:find).with("37").returns(@event)
+    it "@event に期待したイベントが保持される" do
+      Event.expects(:find).with("37").returns(@kato_event)
       get :edit, :id => "37"
-      assigns(:event).should == @event
+      assigns(:event).should == @kato_event
+    end
+    it "自分が主催しているイベントしか編集できない" do 
+      Event.stubs(:find).with("37").returns(@wada_event)
+      (get :edit, :id => "37").should redirect_to events_path
     end
   end
 
   describe "PUT cancel_event" do 
-    it "参加しているイベントに対してキャンセルできる" do
-      Event.stubs(:find).with("10").returns(@event)
-      @event.expects(:cancel).with(@kato)
-      put :cancel, :id => "10"
+    before do 
+      Event.stubs(:find).with("10").returns(@kato_event)
+      @kato_event.expects(:cancel).with(@kato)
     end
-    it "キャンセルしたら、/profile ページにリダイレクト"  do
-      Event.stubs(:find).with("10").returns(@event)
-      @event.expects(:cancel).with(@kato)
+    it "キャンセルしたら、プロフィールページにリダイレクト"  do
       (put :cancel, :id => "10").should redirect_to(profile_path)
     end
+
   end
 
-  ## PENDING ...
-  pending do
   describe "POST create" do
-    describe "with valid params" do
-      it "assigns a newly created event as @event" do
-        login_as(@kato)
-        post :create, :event => @event
-        assigns(:event).should == @event
+    describe "入力した内容に問題がなかった場合" do
+      before do 
+        @normal_event = Factory.build(:event, :name => 'normal')
+        post :create, :event => @normal_event.attributes
       end
-
-      it "redirects to the created event" do
-        login_as(@kato)
-        post :create, :event => @event
-        response.should redirect_to(event_path(@event))
+      it "自動的にログインユーザが主催者とされる" do 
+        assigns(:event).organizer.should == @kato 
+      end
+      it "主催イベントの参照ページに遷移する" do 
+        response.should redirect_to for_organizer_event_path(assigns(:event))
       end
     end
 
-    describe "with invalid params" do
-      it "assigns a newly created but unsaved event as @event" do
-        Event.stub(:new).with({'these' => 'params'}) { mock_event(:save => false) }
-        post :create, :event => {'these' => 'params'}
-        assigns(:event).should be(mock_event)
+    describe "入力した内容に問題があった場合" do
+      before do 
+        Event.any_instance.expects(:save).returns(false)
       end
+      subject { post :create, :event => { :name => 'invalid' } }
+      it { should render_template("new") }
+    end
 
-      it "re-renders the 'new' template" do
-        Event.stub(:new) { mock_event(:save => false) }
-        post :create, :event => {}
-        response.should render_template("new")
+    describe "自分が主催するイベントしか登録できない" do 
+      it "プロフィールページにリダイレクトする" do 
+        pending "自分が主催するイベント以外を POST するような状況がよくわからない"
+        oda = Factory.create(:member, :name => 'oda')
+        post :create, :event => Factory.attributes_for(:event, :organizer => oda)
+        response.should redirect_to profile_path
       end
     end
   end
 
   describe "PUT update" do
-    describe "with valid params" do
-      it "updates the requested event" do
-        Event.stub(:find).with("37") { mock_event }
-        mock_event.should_receive(:update_attributes).with({'these' => 'params'})
-        put :update, :id => "37", :event => {'these' => 'params'}
+    describe "入力した内容に問題がなかった場合" do
+      before do 
+        @normal_event = Factory.build(:event, :name => 'normal',
+                                      :place => 'where')
+        Event.stubs(:find).with("37").returns(@normal_event)
+      end
+      it "Event#update_attributes が呼び出される" do
+        @normal_event.expects(:update_attributes).with({'place' => 'here'})
+        put :update, :id => "37", :event => {'place' => 'here'}
       end
 
-      it "assigns the requested event as @event" do
-        Event.stub(:find) { mock_event(:update_attributes => true) }
-        put :update, :id => "1"
-        assigns(:event).should be(mock_event)
+      it "@event の内容が更新される" do
+        put :update, :id => "37", :event => { 'place' => 'here' }
+        assigns(:event).place.should == 'here'
       end
 
-      it "redirects to the event" do
-        Event.stub(:find) { mock_event(:update_attributes => true) }
-        put :update, :id => "1"
-        response.should redirect_to(event_url(mock_event))
+      it "主催者用のイベント参照ページにリダイレクト" do
+        put :update, :id => "37", :event => { 'place' => 'here' }
+        response.should redirect_to for_organizer_event_path(assigns(:event))
       end
     end
 
-    describe "with invalid params" do
-      it "assigns the event as @event" do
-        Event.stub(:find) { mock_event(:update_attributes => false) }
-        put :update, :id => "1"
-        assigns(:event).should be(mock_event)
+    describe "入力した内容に問題がある場合" do
+      before do 
+        @invalid_event = Factory.build(:event, :name => 'invalid')
+        Event.stubs(:find).with("42").returns(@invalid_event)
+        @invalid_event.stubs(:update_attributes).returns(false)
       end
 
-      it "re-renders the 'edit' template" do
-        Event.stub(:find) { mock_event(:update_attributes => false) }
-        put :update, :id => "1"
+      it "edit テンプレートにより再レンダリング" do
+        put :update, :id => "42", :event => { 'place' => 'here' }
         response.should render_template("edit")
       end
     end
 
-
-    it "主催者だけがイベントを削除できる"
-    it "主催者だけがイベントを変更できる" do 
-      org = Factory.create(:member, :name => 'org')
-      my_event = Factory.create(:event, :organizer => org, :contents => 'my event')
-      ApplicationController.stub(:current_member).and_return(kato)
-      my_event.update_attributes(:contents => 'kato\'s event!').should be_false
+    describe "他人のイベントは更新できない" do 
+      it "プロフィールページにリダイレクトされる" do 
+        pending "自分が主催するイベント以外を PUT するような状況がよくわからない"
+        Event.stubs(:find).with("42").returns(@wada_event)
+        put :update, :id => "42", :event => { 'place' => 'here' }
+        response.should redirect_to profile_path
+      end
     end
-    it "メンバーは自分が参加しているイベントをキャンセルすることができる" do 
-      pending "EventsController に cancel メソッドを追加するまでペンディング"
-    end
-    it "メンバーは他人の参加をキャンセルすることはできない"
-    it "自分が参加していないイベントをキャンセルしても何も変わらない"
 
   end
-  
+
   describe "DELETE destroy" do
-    it "destroys the requested event" do
-      Event.stub(:find).with("37") { mock_event }
-      mock_event.should_receive(:destroy)
-      delete :destroy, :id => "37"
+    context "Event#destroy が呼び出される" do 
+      before do 
+        @wrong_event = Factory.build(:event, :name => 'wrong event', :organizer => @kato)
+        Event.stubs(:find).with("70").returns(@wrong_event)
+        @wrong_event.expects(:destroy)
+      end
+      subject { delete :destroy, :id => "70" }
+      it { should redirect_to my_events_path }
     end
 
-    it "redirects to the events list" do
-      Event.stub(:find) { mock_event }
-      delete :destroy, :id => "1"
-      response.should redirect_to(events_url)
+    context "参加者がいても主催しているイベントを削除できる。
+参加者がいる場合はもう少し親切な設計にするべきだが、今は問答無用で消せる" do 
+      before  do
+        @event = Factory.create(:event, :name => 'event', :organizer => @kato)
+        @entry = Factory.create(:entry, :event => @event, :member => @wada)
+      end
+      subject { delete :destroy, :id => @event.id }
+      it { should redirect_to my_events_path }
     end
-  end
-
   end
 end
