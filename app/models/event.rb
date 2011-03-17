0254cb839a0c07327376600e862ccc6d168e5d66
koko_u <kozaki.tsuneaki@gmail.com>
2011-03-17 13:02:27 +0900

    イベントを削除した場合、そのイベントへの参加情報(Entry)も併せて削除されるよう変 更

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  has_many :entries
+  has_many :entries, :dependent => :destroy
   belongs_to :organizer, :class_name => "Member", :foreign_key => "organizer"
 
   validates :organizer, :presence => true
